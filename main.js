aac912226f5a03ae37a369f4a91fecddc3b1e94a
abhijitrayarao <abhijitrayarao@gmail.com>
2024-11-19 12:11:10 +0530

    commit001

--- main.js
+++ main.js
@@ -25,13 +25,18 @@ class carousel extends HTMLElement {
     this.prevButton = this.shadowRoot.querySelector(".prev");
     this.nextButton = this.shadowRoot.querySelector(".next");
     this.prevClicked = 0;
+    this.nextClicked = this.images.length - 1;
     this.prevButton.addEventListener("click", (e) =>
       this.prevFunction(e, this.images)
     );
     this.nextButton.addEventListener("click", (e) =>
       this.nextFucntion(e, this.images)
     );
+    this.images.forEach((image) => {
+      image.style.transform = "translateX(0px)";
+    });
   }
+
   prevFunction(e, images) {
     let len = images.length;
     if (this.prevClicked === len - 1) {
@@ -50,12 +55,32 @@ class carousel extends HTMLElement {
         }px)`;
       }
     });
-    console.log(this.prevClicked);
-
     this.prevClicked++;
   }
-  nextFucntion(images) {
-    console.log(4545);
+
+  nextFucntion(e, images) {
+    let len = images.length;
+    if (this.prevClicked === 0) {
+      // TODO:write this condition
+      return;
+    }
+    images.forEach((_, i) => {
+      console.log(images[i].style.transform, this.prevClicked, i);
+      if (i === this.prevClicked) {
+        let val = images[0].style.transform.split("(")[1].split(")")[0];
+
+        console.log(`${-600 * i - 15 * i - parseInt(val)}`);
+
+        images[i].style.transform = `translateX(${
+          -600 * i - 15 * i - parseInt(val)
+        }px)`;
+
+        images[i - 1].style.transform = `translateX(${
+          -600 * i - 15 * i - parseInt(val)
+        }px)`;
+      }
+    });
+    this.prevClicked--;
   }
   connectedCallback() {
     this.render();
